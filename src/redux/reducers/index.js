48ecadbe7a32a6bd8c40c7797cbb8631bf3eb095
prmstrgt <pramestiregitaa@gmail.com>
2020-10-12 09:40:07 +0700

    attach new reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,15 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth';
+import product from './product';
+import profile from './profile';
+import cart from './cart';
+import checkout from './checkout';
 
 export default combineReducers({
   auth,
+  product,
+  profile,
+  cart,
+  checkout,
 });
